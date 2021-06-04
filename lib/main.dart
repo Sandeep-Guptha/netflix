68eb51c26a8b0a490ed0ce3cabac916c264d53c9
Sandeep Guptha <sandeepguptha9@gmail.com>
2021-06-04 19:08:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset(
+            "Assets/netflix big logo.png",
+            height: 50,
+          ),
+        ),
+        body: Forms(),
+      ),
+    );
+  }
+}
+
+class Forms extends StatefulWidget {
+  @override
+  _FormsState createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  final _keys = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _keys,
+      child: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            child: Text(
+              "Hello",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
