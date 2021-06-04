1f242796c7af808b26be38cad71c34009c36fe9f
Sandeep Guptha <sandeepguptha9@gmail.com>
2021-06-04 23:20:06 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,42 +12,120 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Image.asset(
-            "Assets/netflix big logo.png",
-            height: 50,
-          ),
+        backgroundColor: Colors.grey.shade800,
+        body: Stack(
+          children: [
+            SingleChildScrollView(),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              height: 800,
+              color: Colors.black,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              height: 800,
+              child: Image.asset(
+                "Assets/netflix-image.webp",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              height: 800,
+              color: Colors.black38,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: Image.asset(
+                "Assets/netflix logo.jpg",
+                height: 30,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 710, left: 5),
+              width: 350,
+              height: 50,
+              // ignore: deprecated_member_use
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Second()),
+                  );
+                },
+                color: Colors.red.shade900,
+                child: Text(
+                  "GET STARTED",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 200),
+              alignment: Alignment.center,
+              child: Text(
+                "Unlimited",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 300),
+              alignment: Alignment.center,
+              child: Text(
+                "Entertainment,",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 390),
+              alignment: Alignment.center,
+              child: Text(
+                "one low price.",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 500),
+              alignment: Alignment.center,
+              child: Text(
+                "Everything on Netflix,",
+                style: TextStyle(color: Colors.white70, fontSize: 18),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 550),
+              alignment: Alignment.center,
+              child: Text(
+                "Starting at just Rs.199/-",
+                style: TextStyle(color: Colors.white70, fontSize: 18),
+              ),
+            ),
+          ],
         ),
-        body: Forms(),
       ),
     );
   }
 }
 
-class Forms extends StatefulWidget {
-  @override
-  _FormsState createState() => _FormsState();
-}
-
-class _FormsState extends State<Forms> {
-  final _keys = GlobalKey<FormState>();
+class Second extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _keys,
-      child: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-            child: Text(
-              "Hello",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
-      )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+      ),
     );
   }
 }
