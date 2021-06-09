18fa957dbec720d06807055bcb120b5df93e5004
Sandeep Guptha <sandeepguptha9@gmail.com>
2021-06-09 21:14:59 +0530

    commit

--- lib/SignInpage.dart
+++ lib/SignInpage.dart
@@ -1,12 +1,19 @@
 //import 'dart:ffi';
 
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:netflix/Home.dart';
 import 'package:netflix/SignIn.dart';
 
 //import 'dart:html';
 
-class SignInpage extends StatelessWidget {
+class SignInpage extends StatefulWidget {
+  @override
+  _SignInpageState createState() => _SignInpageState();
+}
+
+class _SignInpageState extends State<SignInpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
